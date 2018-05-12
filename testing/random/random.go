f47617a3e0ead127f0dca2dcab3399b591d8c2bf
peter.gallerani@gmail.com <peter.gallerani@gmail.com>
2018-05-11 23:21:47 -0600

    first commit

---
+++ testing/random/random.go
@@ -0,0 +1,11 @@
+package random
+
+// ReturnHelloWorld returns hello world.
+func ReturnHelloWorld() string {
+	return "hello world"
+}
+
+// ReturnPassedString will return string argument
+func ReturnPassedString(s string) string {
+	return s
+}
---
+++ testing/random/random_test.go
@@ -0,0 +1,24 @@
+package random
+
+import (
+	"reflect"
+	"testing"
+)
+
+// TestPrintHelloWorld is a simple test for testing the return of a function.
+func TestReturnHelloWorld(t *testing.T) {
+	result := ReturnHelloWorld()
+	if result != "hello world" {
+		t.Fatal("hello world was not returned")
+	}
+}
+
+func TestReturnPassedString(t *testing.T) {
+	result := ReturnPassedString("peter")
+	if result != "peter" {
+		t.Fatal("peter was not returned")
+	}
+	if reflect.TypeOf(result) != reflect.TypeOf("peter") {
+		t.Fatal("type returned was not of type string")
+	}
+}
